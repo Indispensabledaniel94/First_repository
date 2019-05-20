bc69e631c5698fad7a8a00c91cab1796a2e3b9a6
indispenbledaniel94 <indispensabledaniel@gmail.com>
2019-05-20 15:15:39 +0000

    first commit

---
+++ Mysecondcode.py
@@ -0,0 +1,13 @@
+name="Awesome"
+fruit="mango"
+day="Monday"
+print("Hello",name,"I know you love",fruit,"I will buy",4*5,"while coming back on",day)
+
+name="Friday"
+day="friday"
+moment="free"
+food="rice"
+meat="chicken"
+drink="pepsi"
+print("Hey",name,"I will be coming to the house on",day,"to have a",moment,"mement,and i will like to eat",food,"and",meat,"and also a very cold",drink,". Please make sure that all this things are open")
+# First_repository
